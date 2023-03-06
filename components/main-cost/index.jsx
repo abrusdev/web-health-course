b9ce5758dfc8a1d23a10d559465a6cf9d58b2837
abrusdev <abrusdev@gmail.com>
2023-03-06 22:28:14 +0500

    add: some last changes

--- components/main-cost/index.jsx
+++ components/main-cost/index.jsx
@@ -4,6 +4,7 @@ import MainCostTab from "@/components/main-cost/tab.index";
 import { useState } from "react";
 import MainCostTariff from "@/components/main-cost/tariff";
 import data from "./data.json";
+import Image from "next/image";
 
 function MainCost() {
   const [selectedTab, setSelectedTab] = useState(-1);
@@ -24,16 +25,27 @@ function MainCost() {
   }
 
   return (
-    <Container className={classes.content}>
-      <h2>Узнайте стоимость программы</h2>
-
-      <div className={classes.tabsContent}>
-        <MainCostTab title='Для физических лиц' isActive={selectedTab === 0} onClick={() => handleTabSelect(0)} />
-        <MainCostTab title='Для юридических лиц/ ИП' isActive={selectedTab === 1} onClick={() => handleTabSelect(1)} />
+    <div className={classes.content}>
+      <div className={classes.cover}>
+        <Image src="/images/cover_main_4.png" alt="cover" width={178} height={356} />
       </div>
 
-      <MainCostTariff tariff={data.tariffs[selectedTab] ?? data.tariffs[0]} isVisible={selectedTab !== -1} />
-    </Container>
+      <div className={classes.cover2}>
+        <Image src="/images/cover_main_5.png" alt="cover" width={410} height={617} />
+      </div>
+
+      <Container className={classes.innerContent}>
+        <h2>Узнайте стоимость программы</h2>
+
+        <div className={classes.tabsContent}>
+          <MainCostTab title='Для физических лиц' isActive={selectedTab === 0} onClick={() => handleTabSelect(0)} />
+          <MainCostTab title='Для юридических лиц/ ИП' isActive={selectedTab === 1}
+                       onClick={() => handleTabSelect(1)} />
+        </div>
+
+        <MainCostTariff tariff={data.tariffs[selectedTab] ?? data.tariffs[0]} isVisible={selectedTab !== -1} />
+      </Container>
+    </div>
   )
 }
 
