8272a7dd3f21969ab33f15c211d1c411bbc084b3
abrusdev <abrusdev@gmail.com>
2023-03-08 00:02:33 +0500

    update: logic of MainQuestionsComponent

--- components/main-questions/item/index.jsx
+++ components/main-questions/item/index.jsx
@@ -1,10 +1,35 @@
 import classes from './index.module.css';
+import { useEffect, useRef, useState } from "react";
 
 function MainQuestionItem({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [contentHeight, setContentHeight] = useState();
+
+  const templateRef = useRef();
+
+  useEffect(() => {
+    const height = templateRef.current.clientHeight + 40
+
+    setContentHeight(isOpen ? height : 0);
+  }, [isOpen])
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
 
   return (
-    <div className={classes.content}>
-      <p className={classes.title}>Как записаться на онлайн-консультацию с врачом?</p>
+    <div className={classes.content} onClick={handleClick}>
+      <p className={classes.template} style={{ height: "auto" }} ref={templateRef}>
+        <span>{item.content}</span>
+      </p>
+
+      <div className={classes.titleContent}>
+        <p className={classes.title}>{item.title}</p>
+      </div>
+
+      <p className={classes.desc} style={{ height: contentHeight }}>
+        <span>{item.content}</span>
+      </p>
     </div>
   )
 }
