db07c21ee7cbb0b3a126abc35f48a939faa189f0
abrusdev <abrusdev@gmail.com>
2023-03-08 01:34:18 +0500

    update: MainCostRegistration component

--- components/input/index.jsx
+++ components/input/index.jsx
@@ -1,7 +1,9 @@
 import classes from "./index.module.css";
 
-function Input() {
-
+function Input({ width, mt }) {
+  return (
+    <input className={classes.input} style={{ width: width, marginTop: mt }} />
+  )
 }
 
 export default Input
--- components/main-cost/registration/index.jsx
+++ components/main-cost/registration/index.jsx
@@ -11,87 +11,82 @@ function MainCostRegistration({ isVisible }) {
         <p className={classes.desc}>для физических лиц тариф Базовый</p>
 
         <div className={classes.statusesContent}>
-          <MainCostRegistrationStatus step={1} title='Выбор тарифа' isPassed width={226}/>
-          <MainCostRegistrationStatus step={2} title='Оформление' isPassed width={232}/>
+          <MainCostRegistrationStatus step={1} title='Выбор тарифа' isPassed width={226} />
+          <MainCostRegistrationStatus step={2} title='Оформление' isPassed width={232} />
           <MainCostRegistrationStatus step={3} title='Подтверждение и оплата' isPassed={false} width={346} />
         </div>
 
-        <h3>Данные по Страхователю</h3>
+        <h3 className={classes.miniTitle} style={{ marginTop: 25 }}>Данные по Страхователю</h3>
 
-        <div>
-
+        <div className={classes.withTwoItems} style={{ marginTop: 24 }}>
           <div>
-            <div>
-              <p>ФИО*</p>
-              <Input />
-            </div>
-
-            <div>
-              <p>Дата рождения*</p>
-              <Input />
-            </div>
+            <p className={classes.label}>ФИО*</p>
+            <Input width={524} mt={8} />
           </div>
 
           <div>
-            <p>Адрес</p>
-            <Input />
+            <p>Дата рождения*</p>
+            <Input width={194} mt={8} />
           </div>
-
-          <div>
-            <div>
-              <p>Телефон*</p>
-              <Input />
-            </div>
-
-            <div>
-              <p>Email*</p>
-              <Input />
-            </div>
-          </div>
-
-          <p>Паспортные данные</p>
-          <div>
-            <Input />
-            <Input />
-            <Input />
-          </div>
-
-          <Input />
-
-          <Input />
-
-          <h3>Данные по Застрахованному</h3>
-
-          <div>
-            <div>
-              <p>ФИО*</p>
-              <Input />
-            </div>
-
-            <div>
-              <p>Дата рождения*</p>
-              <Input />
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <p>Телефон*</p>
-              <Input />
-            </div>
-
-            <div>
-              <p>Email*</p>
-              <Input />
-            </div>
-          </div>
-
-          <p>Дата начала действия полиса</p>
-          <Input />
-
-          <button>Отправить данные</button>
-
         </div>
+
+        <p className={classes.label} style={{ marginTop: 24 }}>Адрес</p>
+        <Input width={740} mt={8} />
+
+        <div className={classes.withTwoItems} style={{ marginTop: 24, width: 586 }}>
+          <div>
+            <p className={classes.label}>Телефон*</p>
+            <Input width={184} mt={8} />
+          </div>
+
+          <div>
+            <p className={classes.label}>Email*</p>
+            <Input width={378} mt={8} />
+          </div>
+        </div>
+
+        <p className={classes.label} style={{ marginTop: 24 }}>Паспортные данные</p>
+        <div className={classes.withTwoItems} style={{ marginTop: 24, width: 586 }}>
+          <Input width={154} mt={8} />
+          <Input width={204} mt={8} />
+          <Input width={204} mt={8} />
+        </div>
+
+        <Input width={587} mt={12} />
+
+        <Input width={212} mt={12} />
+
+        <h3 className={classes.miniTitle} style={{ marginTop: 64 }}>Данные по Страхователю</h3>
+
+        <div className={classes.withTwoItems} style={{ marginTop: 24 }}>
+          <div>
+            <p className={classes.label}>ФИО*</p>
+            <Input width={524} mt={8} />
+          </div>
+
+          <div>
+            <p>Дата рождения*</p>
+            <Input width={194} mt={8} />
+          </div>
+        </div>
+
+        <div className={classes.withTwoItems} style={{ marginTop: 24, width: 586 }}>
+          <div>
+            <p className={classes.label}>Телефон*</p>
+            <Input width={184} mt={8} />
+          </div>
+
+          <div>
+            <p className={classes.label}>Email*</p>
+            <Input width={378} mt={8} />
+          </div>
+        </div>
+
+        <p className={classes.label} style={{ marginTop: 24 }}>Дата начала действия полиса</p>
+        <Input width={189} mt={8} />
+
+        <button className={classes.btn}>Отправить данные</button>
+
       </div>
     </div>
   )
