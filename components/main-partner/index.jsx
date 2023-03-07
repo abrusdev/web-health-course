447c5206cf5d936d52774470df380986415ca7f1
abrusdev <abrusdev@gmail.com>
2023-03-07 23:36:52 +0500

    add: last changes with adoption ( part 6 )

--- components/main-partner/index.jsx
+++ components/main-partner/index.jsx
@@ -1,17 +1,19 @@
 import classes from "./index.module.css";
 import { Container } from "@mui/material";
 import Image from "next/image";
+import { useStyles } from "@/pages/context/StyleContext";
 
 function MainPartner() {
+  const { isMobile } = useStyles();
 
   return (
     <Container className={classes.content}>
-      <h2>Партнёр</h2>
+      <h1>Партнёр</h1>
 
       <div className={classes.innerContent}>
-        <Image src="/images/partner_1.png" alt="partner" width={340} height={90} />
+        <Image src="/images/partner_1.png" alt="partner" width={isMobile ? 195 : 348} height={isMobile ? 48 : 90} />
         <div className={classes.phone}>
-          <Image src='/icons/ic_phone.svg' alt='phone' width={27} height={27} />
+          <Image src='/icons/ic_phone.svg' alt='phone' width={isMobile ? 13 : 27} height={isMobile ? 13 : 27} />
           +7 (800) 301-86-44
         </div>
       </div>
